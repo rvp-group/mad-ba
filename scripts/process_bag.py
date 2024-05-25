28399050f3aca51d89a7fd04f8431072c0969e89
Krzysztof Ćwian <krzysztof.cwian@gmail.com>
2024-05-25 08:38:36 +0200

    Added option to process multiple bags by script

--- scripts/process_bag.py
+++ scripts/process_bag.py
@@ -14,7 +14,7 @@ import math
 from tqdm import tqdm
 
 # User parameters
-bag_file_path = './bag/2021-07-01-10-37-38-quad-easy.bag'
+bag_file_paths = ['./bag/2021-07-01-10-37-38-quad-easy.bag']
 tum_file_path = './init_tum/loam_opensource.txt'
 odom_time_offset = 0.01
 # User parameters for added topic
@@ -22,25 +22,30 @@ topic_name = "/loam_opensource"
 frame_id = "map"
 child_frame_id = "loam_opensource"
 
-def reorder_timestamps(rosbag_file_path):
-    try:
-        bag = rosbag.Bag(rosbag_file_path, "r")
-    except rosbag.bag.ROSBagException as e:
-        print(e)
-        if e.value == "empty file":
-            return False
-        else:
-            return False
-    except Exception as e:
-        print(e)
-        return False
+def reorder_timestamps(rosbag_file_paths):
 
-    with rosbag.Bag(rosbag_file_path + ".out", 'w', 'lz4') as outbag:
-        for topic, msg, t in tqdm(bag.read_messages()):
-            if topic == "/tf" and msg.transforms:
-                outbag.write(topic, msg, msg.transforms[0].header.stamp)
+    bags = []
+    for rosbag_file_path in rosbag_file_paths:
+        try:
+            bag = rosbag.Bag(rosbag_file_path, "r")
+            bags.append(bag)
+        except rosbag.bag.ROSBagException as e:
+            print(e)
+            if e.value == "empty file":
+                return False
             else:
-                outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+                return False
+        except Exception as e:
+            print(e)
+            return False
+
+    with rosbag.Bag(rosbag_file_paths[0] + ".out", 'w', 'lz4') as outbag:
+        for bag in bags:
+            for topic, msg, t in tqdm(bag.read_messages()):
+                if topic == "/tf" and msg.transforms:
+                    outbag.write(topic, msg, msg.transforms[0].header.stamp)
+                else:
+                    outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
     return True
 
 def add_odom_topic(rosbag_file_path, tum_file_path, topic_name, frame_id, child_frame_id, odom_time_offset):
@@ -105,7 +110,7 @@ def add_odom_topic(rosbag_file_path, tum_file_path, topic_name, frame_id, child_
 if __name__ == "__main__":
 
     # Reorder messages - creates new .bag.out file
-    if reorder_timestamps(bag_file_path):
-        bag_file_path = bag_file_path + ".out"
+    if reorder_timestamps(bag_file_paths):
+        merged_bag_file_path = bag_file_paths[0] + ".out"
         # Add odom topic
-        add_odom_topic(bag_file_path, tum_file_path, topic_name, frame_id, child_frame_id, odom_time_offset)
+        add_odom_topic(merged_bag_file_path, tum_file_path, topic_name, frame_id, child_frame_id, odom_time_offset)
