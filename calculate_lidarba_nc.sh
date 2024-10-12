303a8ae42f99af6ac4ff601b50b088b5498b0de3
Krzystof Ćwian <krzysztof.cwian@gmail.com>
2024-10-12 15:08:21 +0000

    Added calculate script

---
+++ calculate_lidarba_nc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rosrun structure_refinement main_app -c config/NewerCollege/quad_easy.config
+rosrun structure_refinement main_app -c config/NewerCollege/maths_institute.config
+rosrun structure_refinement main_app -c config/NewerCollege/stairs.config
+rosrun structure_refinement main_app -c config/NewerCollege/underground_mine_easy.config
