91f486b8dc52501dd976ee327b587ae40f880c31
Krzystof Ćwian <krzysztof.cwian@gmail.com>
2024-10-12 15:49:05 +0000

    Corrected script

--- calculate_lidarba_vbr.sh
+++ calculate_lidarba_vbr.sh
@@ -5,5 +5,3 @@ rosrun structure_refinement main_app -c config/VBR/colosseo.config
 rosrun structure_refinement main_app -c config/VBR/diag.config
 rosrun structure_refinement main_app -c config/VBR/pincio.config
 rosrun structure_refinement main_app -c config/VBR/spagna.config
-
-campus.config  ciampino.config  colosseo.config  diag.config  pincio.config  spagna.config
