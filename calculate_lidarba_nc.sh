cbf4d16b5177113de2878740741db1f80e21be93
Krzysztof Ćwian <krzysztof.cwian@gmail.com>
2025-05-15 12:17:13 +0200

    Removed unnecesarry folders

--- calculate_lidarba_nc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rosrun mad_ba main_app -c config/NewerCollege/quad_easy.config
-rosrun mad_ba main_app -c config/NewerCollege/maths_institute.config
-rosrun mad_ba main_app -c config/NewerCollege/stairs.config
-rosrun mad_ba main_app -c config/NewerCollege/underground_mine_easy.config
-rosrun mad_ba main_app -c config/NewerCollege/cloister.config
--- calculate_lidarba_vbr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rosrun mad_ba main_app -c config/VBR/campus.config
-rosrun mad_ba main_app -c config/VBR/ciampino.config
-rosrun mad_ba main_app -c config/VBR/colosseo.config
-rosrun mad_ba main_app -c config/VBR/diag.config
-rosrun mad_ba main_app -c config/VBR/pincio.config
-rosrun mad_ba main_app -c config/VBR/spagna.config
